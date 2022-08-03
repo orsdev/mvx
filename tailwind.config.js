3943a0eeffd0975f2325b0cfe87d8fb6de4bc7ed
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2022-08-03 17:47:30 +0100

    🎉 feat: add app initial structure

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        secondary: "#FF8A00",
+        primary: "#005ECD",
+        dark: "#333333",
+      },
+    },
+  },
+  plugins: [],
+};
