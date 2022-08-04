c8699c02d1f7226682092bd59c7ddac4f482127d
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2022-08-04 16:15:40 +0100

    🔧 feat: tailwind file updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
+    screens: {
+      xxs: "390px",
+      xs: "400px",
+      base: "500px",
+      ...defaultTheme.screens,
+      "2md": "950px",
+      "2xl": "1350px",
+    },
     extend: {
       colors: {
         secondary: "#FF8A00",
         primary: "#005ECD",
         dark: "#333333",
+        accent: "#5D2A42",
+        hover: {
+          blue: "rgba(217, 231, 248, 0.3)",
+          orange: "rgba(255, 195, 129, 0.2)",
+        },
+      },
+      fontFamily: {
+        montMedium: "medium",
+        montLight: "light",
+        montSemiBold: "semibold",
+        catalina: "catalina",
       },
     },
   },
